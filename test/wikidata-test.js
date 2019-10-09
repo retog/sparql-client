1729cfe5648f691a9a05df533a7e1a9b90e5eec3
Reto Gmür <reto@factsmission.com>
2019-10-09 15:14:24 +0200

    increased timeout to 15s
    (slow wikidata shouldn't cause failure)

--- test/wikidata-test.js
+++ test/wikidata-test.js
@@ -4,10 +4,10 @@ import assert from "assert"
 describe("SparqlEndpoint", function() {
     let sparqlEndpoint = new SparqlEndpoint("https://query.wikidata.org/sparql")
     describe("getSparqlResultSet", function() {
-        it("should get result with head nad bindings", async function() {
+        it("should get result with head and bindings", async function() {
             const res = await sparqlEndpoint.getSparqlResultSet("SELECT ?height WHERE { wd:Q243 wdt:P2048 ?height . }");
             assert.ok(res.head.vars.length)
             assert.ok(res.results.bindings.length)
-        });
+        }).timeout(15000);
     });
   });
