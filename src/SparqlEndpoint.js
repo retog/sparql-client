721fe1e2cbc7fccc44af54094e98d2d65da01529
Reto Gmür <reto@factsmission.com>
2020-05-02 22:52:54 +0200

    trying with ne.js instead of ext-rdflib

--- src/SparqlEndpoint.js
+++ src/SparqlEndpoint.js
@@ -1,4 +1,4 @@
-import $rdf from "ext-rdflib";
+import N3 from "n3";
 import fetch from "node-fetch"
 
 export default class SparqlEndpoint {
@@ -26,9 +26,10 @@ export default class SparqlEndpoint {
     }
     
     getSparqlRDF(query, fetchOptions = {}) {
+        const parser = new N3.Parser();
         if (this._logQueries) console.log(query);
         let encodedQuery = encodeURIComponent(query);
-        return $rdf.rdfFetch(this._uri + "?query=" + encodedQuery, {
+        return fetch(this._uri + "?query=" + encodedQuery, {
             ...fetchOptions,
             headers: new Headers({
                 accept: "text/turtle"
@@ -38,9 +39,11 @@ export default class SparqlEndpoint {
             if (!response.ok) {
                 throw response.status;
             } else {
-                return response.graph();
+                return response.text();
             }
-        });
+        }).then(text => {
+            return parser.parse(text);
+        })
     }
 }
 
